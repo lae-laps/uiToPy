e05543aaa46f8f6302b6daa85c2325a3214c52df
laelaps <77897569+lae-laps@users.noreply.github.com>
2021-07-06 17:41:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,92 +1,42 @@
-import os
+import amino
 
-color_dict = {
-    "red": "196",
-    "green": "149",
-    "orange": "208"
+chat_lobby_erajurasica = '69688ab3-20cc-4b24-948d-532806c805cc'
+chat_isla_tartaros = '64ce2b86-e4a1-42c8-bc7e-95a1e976199e'
+
+chats = ['64ce2b86-e4a1-42c8-bc7e-95a1e976199e', '69688ab3-20cc-4b24-948d-532806c805cc']
+
+print()
+print('=========')
+print('THOTH-BOT')
+print('=========')
+
+
+creds = {
+    'email':"cifehij223@geekale.com",
+    'password':"thothbotG36"
 }
 
-# METHODS ================================= #
-
-def printf(content, color):
-    color_code = color_dict[color]
-    color_code = color_code + "m"
-    to_print_start = "\033[38;5;"
-    to_print_end = "\033[m"
-    to_print = to_print_start + color_code + content + to_print_end
-    print(to_print)
-
-def get_printf_str(content, color):
-    color_code = color_dict[color]
-    color_code = color_code + "m"
-    to_print_start = "\033[38;5;"
-    to_print_end = "\033[m"
-    to_print = to_print_start + color_code + content + to_print_end
-    return to_print
-
-def switch_case(content):
-    global path
-
-    content = content[1:]
-    if content == "exit":
-        sure_str = get_printf_str("¿Seguro que quieres acabar el proceso? - [y/N]", "orange")
-        sure = input(sure_str)
-        if sure == "y":
-            print()
-            printf("Saliendo...", "orange")
-            exit()
-
-    elif content == "help":
-        print('''
-               Uso de esta Tool
-
-               /convert - Convierte de .ui a .py
-               /help - Da este texto
-               /exit - Cierra la tool
-
-               ~ Created by: Laelaps ~
-               (github.com/lae-laps) - For contact
-               ''')
-        return True
-
-    elif content == "convert":
-        new_name_str = get_printf_str("Nuevo nombre del fichero (con extensión .py): ", "orange")
-        name = input(new_name_str)
-        os.system("pyuic5 -x " + path + " -o " + name)
-        print()
-        printf("El fichero se convirtió a .py", "green")
-        print()
-        mv_org_str = get_printf_str("¿Mover .py a directorio original? ~ [y/N]", "orange")
-        mv_org = input(mv_org_str)
-        if mv_org == "y":
-            path_to_mv = path.split("/")
-            path_to_mv.pop()
-            final_path = "/".join(path_to_mv)
-            os.system("mv " + name + " " + final_path)
-        return True
-
-    else:
-        return False
-
-
-printf("===========", "green")
-printf(".UI ==> .PY", "green")
-printf("===========", "green")
-print()
-
-path_in_string = get_printf_str("Path absoluto del fichero .ui: ", "green")
-path = input(path_in_string)
+client = amino.Client()
+client.login(**creds)
 
 print()
-printf("Usa /help para ver como se usa la tool", "green")
+print("Logged in as", client.profile.nickname)
 print()
 
-# MAIN ========================================= #
+comid = '260391359'
 
-while True:
-    in_exe_str = get_printf_str("[/usr/lib] ~$ ", "green")
-    exe = input(in_exe_str)
-    is_instruction = switch_case(exe)
-    if is_instruction == False:
-        printf("~ Ese comando no existe ~ Usa '/help' para ver los comandos", "red")
-        continue
+subclient = amino.SubClient(comId=comid, profile=client.profile)
+
+print()
+print('Loged into comunity')
+
+def enviar_mensaje(mensaje, chat):
+    subclient.send_message(message=mensaje, messageType=109, chatId=chat)
+
+mensaje = input("Mensaje: ")
+veces = int(input("Número de veces: "))
+
+num_chats = len(chats)
+for x in range(veces):
+    for y in range(num_chats):
+        enviar_mensaje(mensaje, chats[y])
